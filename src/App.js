de47902f046dddea1dac8e6960691e3e74d332e5
Aditya Cheta <adi971113@gmail.com>
2023-04-05 13:09:07 +0530

    React Routes added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Products from "./components/Products";
+import Contact from "./components/Contact";
+import Cart from "./components/Cart";
+import SingleProduct from "./components/SingleProduct";
+import ErrorPage from "./components/ErrorPage";
 
 const App = () => {
-  return <div>Welcome to Thapa React E-Commerce Website</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/singleproduct/:id" element={<SingleProduct />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
