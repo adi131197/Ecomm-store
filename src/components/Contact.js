376700712600dc87e4107406d737c1de15516a98
Aditya Cheta <adi971113@gmail.com>
2023-04-06 12:23:47 +0530

    Contact and Error Page added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -33,7 +33,51 @@ const Contact = () => {
     }
   `;
 
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <h2 className="common-heading">Feel Free to Contact Us</h2>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d236212.9644141817!2d73.03299813982856!3d22.322360101127163!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395fc8ab91a3ddab%3A0xac39d3bfe1473fb8!2sVadodara%2C%20Gujarat!5e0!3m2!1sen!2sin!4v1680762284079!5m2!1sen!2sin"
+        width="100%"
+        height="400"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        title="Map"
+      ></iframe>
+
+      <div className="container">
+        <div className="contact-form">
+          <form action="#" method="post" className="contact-inputs">
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              required
+              autoComplete="off"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="Email"
+              required
+              autoComplete="off"
+            />
+            <textarea
+              name="message"
+              cols="30"
+              rows="10"
+              required
+              autoComplete="off"
+              placeholder="Enter your message"
+            ></textarea>
+            <input type="submit" value="Send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Contact;
