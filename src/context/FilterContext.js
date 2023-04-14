e743de6f136dbab2ea143c6f2510437e1902120f
Aditya Cheta <adi971113@gmail.com>
2023-04-14 12:22:54 +0530

    Filters Added

--- src/context/FilterContext.js
+++ src/context/FilterContext.js
@@ -1,10 +1,13 @@
 import { createContext, useContext, useEffect, useReducer } from "react";
 import { useProductContext } from "./ProductContext";
 import {
+  CLEAR_FILTERS,
+  FILTER_PRODUCTS,
   LOAD_FILTER_PRODUCTS,
   SET_SORT_BY,
   SORT_PRODUCTS,
   TOGGLE_VIEW,
+  UPDATE_FILTERS_VALUE,
 } from "../constants/constants";
 import reducer from "../reducers/FilterReducer";
 
@@ -15,6 +18,15 @@ const initialState = {
   all_products: [],
   isGridView: false,
   sortBy: "lowest",
+  filters: {
+    text: "",
+    category: "All",
+    company: "All",
+    color: "All",
+    maxPrice: 0,
+    price: 0,
+    minPrice: 0,
+  },
 };
 
 export const FilterContextProvider = ({ children }) => {
@@ -30,16 +42,37 @@ export const FilterContextProvider = ({ children }) => {
     return dispatch({ type: SET_SORT_BY, payload: value });
   };
 
+  const updateFilterValue = (event) => {
+    let { name, value } = event.target;
+
+    return dispatch({ type: UPDATE_FILTERS_VALUE, payload: { name, value } });
+  };
+
+  const clearFilters = () => {
+    dispatch({
+      type: CLEAR_FILTERS,
+    });
+  };
+
   useEffect(() => {
-    return dispatch({ type: SORT_PRODUCTS });
-  }, [state.sortBy]);
+    dispatch({ type: FILTER_PRODUCTS });
+    dispatch({ type: SORT_PRODUCTS });
+  }, [products, state.sortBy, state.filters]);
 
   useEffect(() => {
     dispatch({ type: LOAD_FILTER_PRODUCTS, payload: products });
   }, [products]);
 
   return (
-    <FilterContext.Provider value={{ ...state, ToggleView, sortByFunc }}>
+    <FilterContext.Provider
+      value={{
+        ...state,
+        ToggleView,
+        sortByFunc,
+        updateFilterValue,
+        clearFilters,
+      }}
+    >
       {children}
     </FilterContext.Provider>
   );
--- src/reducers/FilterReducer.js
+++ src/reducers/FilterReducer.js
@@ -1,17 +1,27 @@
 import {
+  CLEAR_FILTERS,
+  FILTER_PRODUCTS,
   LOAD_FILTER_PRODUCTS,
   SET_SORT_BY,
   SORT_PRODUCTS,
   TOGGLE_VIEW,
+  UPDATE_FILTERS_VALUE,
 } from "../constants/constants";
 
 const filterReducer = (state, action) => {
   switch (action.type) {
     case LOAD_FILTER_PRODUCTS:
+      let priceArr = action.payload.map((curElem) => curElem.price);
+      let maxPrice = Math.max(...priceArr);
       return {
         ...state,
         filter_products: [...action.payload],
         all_products: [...action.payload],
+        filters: {
+          ...state.filters,
+          maxPrice,
+          price: maxPrice,
+        },
       };
 
     case TOGGLE_VIEW:
@@ -55,6 +65,73 @@ const filterReducer = (state, action) => {
       return {
         ...state,
         filter_products: newSortData,
+      };
+
+    case UPDATE_FILTERS_VALUE:
+      const { name, value } = action.payload;
+
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          [name]: value,
+        },
+      };
+
+    case FILTER_PRODUCTS:
+      let { all_products } = state;
+      let tempFilterProduct = [...all_products];
+
+      const { text, category, company, color, price } = state.filters;
+
+      if (text) {
+        tempFilterProduct = tempFilterProduct.filter((curElem) => {
+          return curElem.name.toLowerCase().includes(text);
+        });
+      }
+
+      if (category !== "All") {
+        tempFilterProduct = tempFilterProduct.filter((curElem) => {
+          return curElem.category === category;
+        });
+      }
+
+      if (company !== "All") {
+        tempFilterProduct = tempFilterProduct.filter((curElem) => {
+          return curElem.company === company;
+        });
+      }
+
+      if (color !== "All") {
+        tempFilterProduct = tempFilterProduct.filter((curElem) => {
+          return curElem.colors.includes(color);
+        });
+      }
+
+      if (price) {
+        tempFilterProduct = tempFilterProduct.filter(
+          (curElem) => curElem.price <= price
+        );
+      }
+
+      return {
+        ...state,
+        filter_products: tempFilterProduct,
+      };
+
+    case CLEAR_FILTERS:
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          text: "",
+          category: "All",
+          company: "All",
+          color: "All",
+          maxPrice: state.filters.maxPrice,
+          price: state.filters.maxPrice,
+          minPrice: 0,
+        },
       };
 
     default:
